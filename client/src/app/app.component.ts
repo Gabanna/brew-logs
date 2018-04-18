8d9615e086e057ba6a36759ce24d6243a2d5678f
Ronny Gallin <ronny.gallin@bwb.de>
2018-04-18 11:56:53 +0200

    changed to node project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+<<<<<<< HEAD
 import { BrewLogPage } from '../pages/brew-log/brew-log.page';
 import { AuthService } from '../services/auth.service';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import localeFr from '@angular/common/locales/fr';
 
+=======
+
+import { HomePage } from '../pages/home/home';
+>>>>>>> 858e51c... changed to node project
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+<<<<<<< HEAD
   rootPage:any = BrewLogPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, authService: AuthService) {
@@ -29,11 +35,20 @@ export class MyApp {
         }
       });
 
+=======
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+>>>>>>> 858e51c... changed to node project
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+<<<<<<< HEAD
 
+=======
+>>>>>>> 858e51c... changed to node project
     });
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,15 @@
+<<<<<<< HEAD
 import { I18nPipe } from './../pipes/i18n.pipe';
 import { UserComponent } from './../components/user/user.component';
 import { BrewHistoryComponent } from './../components/brew-history/brew-history.component';
+=======
+>>>>>>> 858e51c... changed to node project
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+<<<<<<< HEAD
 import { BrewLogService } from '../services/brewlog.service';
 import { HttpModule } from '@angular/http';
 import { ErrorService } from '../services/error.service';
@@ -19,10 +23,19 @@ import { BrewLogComponent } from '../components/brew-log/brew-log.component';
 import { BrewLogDetailPage } from '../pages/brew-log-detail/brew-log-detail.page';
 import { TaskDetailPage } from '../pages/task-detail/task-detail.page';
 import { BrewHistoryEntryComponent } from '../components/brew-history-entry/brew-history-entry.component';
+=======
+import { HttpModule } from '@angular/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+>>>>>>> 858e51c... changed to node project
 
 @NgModule({
   declarations: [
     MyApp,
+<<<<<<< HEAD
     BrewLogPage,
     BrewLogDetailPage,
     TaskDetailPage,
@@ -31,15 +44,23 @@ import { BrewHistoryEntryComponent } from '../components/brew-history-entry/brew
     BrewHistoryEntryComponent,
     UserComponent,
     I18nPipe
+=======
+    HomePage
+>>>>>>> 858e51c... changed to node project
   ],
   imports: [
     BrowserModule,
     HttpModule,
+<<<<<<< HEAD
+=======
+    HttpClientModule,
+>>>>>>> 858e51c... changed to node project
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+<<<<<<< HEAD
     BrewLogPage,
     BrewLogDetailPage,
     TaskDetailPage
@@ -59,5 +80,15 @@ import { BrewHistoryEntryComponent } from '../components/brew-history-entry/brew
     {provide: LOCALE_ID, useValue: navigator.language }
   ],
 
+=======
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
+  ]
+>>>>>>> 858e51c... changed to node project
 })
 export class AppModule {}
