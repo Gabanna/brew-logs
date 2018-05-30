0a6f5da3bccd979a767f03d7d711afb21a8e65be
netcup1 <netcup1@git.de>
2018-05-30 10:49:32 +0200

    trys

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
     api: {
         ssl: false,
-        host: 'localhost',
+        host: 'brewlogs.spdns.de',
         port: 8080,
         key: '82edf96f-cc58-4cfd-848c-8d88f996758a'
     }
